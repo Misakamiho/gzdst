235035470194e4e50a0b26b52c30ff7909fc9037
MeiGenHata <hatameigenhk@gmail.com>
2021-03-26 20:10:27 +0900

    登录页完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import '@/styles/index.less'
+import 'normalize.css'
+
+import router from '@/router/router.js'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
